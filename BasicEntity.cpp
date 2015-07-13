fec83336ccec7a35e755145cbc7211ada948f802
Chris Bowers <jsbluethree@gmail.com>
2015-07-13 15:14:36 -0400

    whoops

--- BasicEntity.cpp
+++ BasicEntity.cpp
@@ -7,4 +7,4 @@ void BasicEntity::move(float offsetX, float offsetY) { Sprite::move(offsetX, off
 
 void BasicEntity::move(const Vector2f& offset) { Sprite::move(offset); }
 
-const FloatRect& BasicEntity::get_rect() const { return m_sprite.getGlobalBounds(); }
+const FloatRect& BasicEntity::get_rect() const { return getGlobalBounds(); }
