b3829e5910b510ce379cad4b3cf681f19e2a9912
Chris Bowers <jsbluethree@gmail.com>
2015-07-13 01:02:53 -0400

    interface for Quadtree of SceneNodes

---
+++ Quadtree.h
@@ -0,0 +1,45 @@
+// Quadtree.h
+// Chris Bowers
+
+#ifndef __QUADTREE_H__
+#define __QUADTREE_H__
+
+#include "SceneNode.h"
+#include "SFML.h"
+#include "typedefs.h"
+#include <list>
+
+struct Quadtree{
+	Quadtree(u32 node_level, FloatRect node_bounds);
+	~Quadtree();
+	
+	void clear();
+	void insert(ISceneNode* node);
+	std::list<ISceneNode*> retrieve(float x, float y);
+	std::list<ISceneNode*> retrieve(const Vector2f& vec);
+	std::list<ISceneNode*> retrieve(const FloatRect& rect);
+	std::list<ISceneNode*> retrieve(ISceneNode* node);
+	
+private:
+	void split();
+	
+	i8 get_index(float x, float y);
+	i8 get_index(const Vector2f& vec);
+	i8 get_index(const FloatRect& rect);
+	i8 get_index(ISceneNode* node);
+	
+	std::list<ISceneNode*> _retrieve(std::list<ISceneNode*> return_list, float x, float y);
+	std::list<ISceneNode*> _retrieve(std::list<ISceneNode*> return_list, const Vector2f& vec);
+	std::list<ISceneNode*> _retrieve(std::list<ISceneNode*> return_list, const FloatRect& rect);
+	std::list<ISceneNode*> _retrieve(std::list<ISceneNode*> return_list, ISceneNode* node);
+	
+	u32 level;
+	std::list<ISceneNode*> nodes;
+	FloatRect bounds;
+	Quadtree* subtrees[4];
+	
+	static u8 MAX_NODES;
+	static u32 MAX_LEVELS;
+};
+
+#endif // __QUADTREE_H__
