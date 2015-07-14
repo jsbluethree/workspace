f6cac7ead3b5c80124ba17ef05d5fae0a050f321
Chris Bowers <jsbluethree@gmail.com>
2015-07-14 01:27:41 -0400

    simple debug logger

---
+++ Log.cpp
@@ -0,0 +1,30 @@
+// Log.cpp
+// Chris Bowers
+
+/**
+ *	This implements a debug logger - see Log.h
+ */
+ 
+ #include "Log.h"
+ 
+ Log::level = LogLevel::NOT_SET;
+ 
+ void Log::set_level(LogLevel lvl) { level = lvl; }
+ 
+ LogLevel Log::get_level() const { return level; }
+ 
+ bool Log::debug(const std::string& msg) { return log(LogLevel::DEBUG, "DEBUG: " + msg); }
+ 
+ bool Log::info(const std::string& msg) { return log(LogLevel::INFO, "INFO: " + msg); }
+ 
+ bool Log::warning(const std::string& msg) { return log(LogLevel::WARNING, "WARNING: " + msg); }
+ 
+ bool Log::error(const std::string& msg) { return log(LogLevel::ERROR, "ERROR: " + msg); }
+ 
+ bool Log::critical(const std::string& msg) { return log(LogLevel::CRITICAL, "CRITICAL: " + msg); }
+ 
+ bool Log::log(LogLevel lvl, const std::string& msg){
+	 if (lvl < level) return false;
+	 std::cerr << msg;
+	 return true;
+ }
---
+++ Log.h
@@ -0,0 +1,39 @@
+// Log.h
+// Chris Bowers
+
+/**
+ *	Defines a logger for debugging and other purposes. It is a class with only static members and methods.
+ */
+ 
+#ifndef __LOG_H__
+#define __LOG_H__
+
+#include "typedefs.h"
+#include <iostream>
+#include <string>
+
+enum class LogLevel : u8{
+	NOT_SET,
+	DEBUG,
+	INFO,
+	WARNING,
+	ERROR,
+	CRITICAL
+};
+
+struct Log{
+	static void set_level(LogLevel lvl);
+	static LogLevel get_level() const;
+	
+	static bool debug(const std::string& msg);
+	static bool info(const std::string& msg);
+	static bool warning(const std::string& msg);
+	static bool error(const std::string& msg);
+	static bool critical(const std::string& msg);
+	static bool log(LogLevel lvl, const std::string& msg);
+	
+private:
+	static LogLevel level;
+};
+
+#endif // __LOG_H__
