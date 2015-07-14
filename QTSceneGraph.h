d0f67df62cbd9b7f7a677519c68016470c8c349e
Chris Bowers <jsbluethree@gmail.com>
2015-07-14 09:46:08 -0400

    fixing linker errors

--- QTSceneGraph.h
+++ QTSceneGraph.h
@@ -19,17 +19,10 @@ struct QTSceneGraph : ISceneGraph{
 	void add_node(ISceneNode* node);
 	void remove_node(ISceneNode* node);
 	void update_node(ISceneNode* node, float dx, float dy);
-	void update_node(ISceneNode* node, const Vector2f& d);
 	
-	bool check_collision(float x, float y);
-	bool check_collision(const Vector2f& v);
 	bool check_collision(const FloatRect& r);
-	bool check_collision(ISceneNode* node);
 	
-	std::unordered_set<ISceneNode*> get_collision(float x, float y);
-	std::unordered_set<ISceneNode*> get_collision(const Vector2f& v);
 	std::unordered_set<ISceneNode*> get_collision(const FloatRect& r);
-	std::unordered_set<ISceneNode*> get_collision(ISceneNode* node);
 	
 private:
 	Quadtree qtree;
