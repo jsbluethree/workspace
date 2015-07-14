e2fb11a525614145e952b24f89c5a1ad7f2bc2e1
Chris Bowers <jsbluethree@gmail.com>
2015-07-14 09:33:32 -0400

    adding window, main loop

--- tests.cpp
+++ tests.cpp
@@ -5,6 +5,19 @@
  *	This contains various tests. This also contains, for now, the entry point.
  */
 
+#include "SFML.h"
+
 int main(int argc, char** argv){
+	Window main_window(VideoMode(800, 600), "Tests");
+
+	while (main_window.isOpen()){
+		Event event;
+		while (main_window.pollEvent(event)){
+			if (event.type == Event::Closed){
+				main_window.close();
+			}
+		}
+	}
+
 	return 0;
 }
