56a3371b632e67ba19e4fdacc0fe8cf6dc12b073
Chris Bowers <jsbluethree@gmail.com>
2015-07-15 23:18:56 -0400

    tests

--- tests.cpp
+++ tests.cpp
@@ -6,9 +6,39 @@
  */
 
 #include "SFML.h"
+#include "Quadtree.h"
+
+RenderWindow main_window;
+
+void test_quad_tree(){
+	Texture tex_am;
+	if (!tex_am.loadFromFile("amethyst.png")) return;
+	Sprite spr_am;
+	spr_am.setTexture(tex_am);
+
+	while (main_window.isOpen()){
+		Event event;
+		while (main_window.pollEvent(event)){
+			if (event.type == Event::Closed){
+				main_window.close();
+			}
+			else if (event.type == Event::KeyPressed){
+				if (event.key.code == Keyboard::Return){
+					return;
+				}
+			}
+		}
+		main_window.clear();
+		main_window.draw(spr_am);
+		main_window.display();
+	}
+}
 
 int main(int argc, char** argv){
-	Window main_window(VideoMode(800, 600), "Tests");
+	main_window.create(VideoMode(800, 600), "Tests");
+	
+	test_quad_tree();
+	/**/
 
 	while (main_window.isOpen()){
 		Event event;
