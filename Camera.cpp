f9e308ab29ca225719145fd059216fe7ed916265
Chris Bowers <jsbluethree@gmail.com>
2015-07-18 02:22:45 -0400

    camera uses sf::View

--- Camera.cpp
+++ Camera.cpp
@@ -10,8 +10,8 @@
 Camera::Camera(FloatRect init_rect) : cam_rect{ init_rect } {}
 
 void Camera::render_scene(const ISceneGraph& scene, RenderTarget& target, const RenderStates& states) const{
-	auto nodes = scene.get_collision(cam_rect);
-	for (const auto& node : nodes){
+	target.setView(View(cam_rect));
+	for (const auto& node : scene.get_collision(cam_rect)){
 		auto drawable = dynamic_cast<Drawable*>(node);
 		if (drawable) target.draw(*drawable, states);
 	}
