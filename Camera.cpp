72c81cc1215e46b07ad4d420ba84da53c85c8a11
Chris Bowers <jsbluethree@gmail.com>
2015-07-29 05:51:17 -0400

    better interface for camera

--- Camera.cpp
+++ Camera.cpp
@@ -7,13 +7,25 @@
 
 #include "Camera.h"
 
-Camera::Camera(FloatRect source, FloatRect viewport) : source_rect{ source }, viewport_rect{ viewport } {}
+Camera::Camera(FloatRect source, FloatRect viewport) : view{ source }{
+	view.setViewport(viewport);
+}
+
+void Camera::set_source(const FloatRect& rect) { view.reset(rect); }
+
+void Camera::set_viewport(const FloatRect& rect) { view.setViewport(rect); }
+
+FloatRect Camera::get_source() const { return{ view.getCenter() - view.getSize() / 2.0f, view.getSize() }; }
+
+FloatRect Camera::get_viewport() const { return view.getViewport(); }
+
+void Camera::move(float dx, float dy) { view.move(dx, dy); }
+
+void Camera::move(const Vector2f& d) { move(d.x, d.y); }
 
 void Camera::render_scene(const ISceneGraph& scene, RenderTarget& target, const RenderStates& states) const{
-	View view(source_rect);
-	view.setViewport(viewport_rect);
 	target.setView(view);
-	for (const auto& node : scene.get_collision(source_rect)){
+	for (const auto& node : scene.get_collision(get_source())){
 		auto drawable = dynamic_cast<Drawable*>(node);
 		if (drawable) target.draw(*drawable, states);
 	}
--- Camera.h
+++ Camera.h
@@ -15,10 +15,16 @@ struct Camera{
 	explicit Camera(FloatRect source, FloatRect viewport = { 0.0f, 0.0f, 1.0f, 1.0f });
 	virtual ~Camera() = default;
 	
+	void set_source(const FloatRect& rect);
+	void set_viewport(const FloatRect& rect);
+	FloatRect get_source() const;
+	FloatRect get_viewport() const;
+	void move(float dx, float dy);
+	void move(const Vector2f& d);
 	void render_scene(const ISceneGraph& scene, RenderTarget& target, const RenderStates& states = RenderStates::Default) const;
 
-	FloatRect source_rect;
-	FloatRect viewport_rect;
+private:
+	View view;
 };
 
 #endif // __CAMERA_H__
