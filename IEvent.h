75011c5f3c9655a4f2cc2f5090f3fb31849ee969
Chris Bowers <jsbluethree@gmail.com>
2015-07-31 06:19:22 -0400

    won't work - sizeof will be wrong

--- IEvent.h
+++ IEvent.h
@@ -9,21 +9,8 @@
 #ifndef __IEVENT_H__
 #define __IEVENT_H__
 
-#ifdef EVENT_POOL_ALLOCATION
-#include "PoolAllocator.h"
-#endif // EVENT_POOL_ALLOCATION
-
 struct IEvent{
 	virtual ~IEvent() = default;
-
-#ifdef EVENT_POOL_ALLOCATION
-
-	void* operator new(size_t){ PoolAllocator<IEvent>().allocate(1); }
-	void* operator new[](size_t n) { PoolAllocator<IEvent>().allocate(n / sizeof(IEvent)); }
-	void operator delete(void* p){ PoolAllocator<IEvent>().deallocate((IEvent*)p, 1); }
-	void operator delete[](void* p, size_t n){ PoolAllocator<IEvent>().deallocate((IEvent*)p, n / sizeof(IEvent)); }
-
-#endif // EVENT_POOL_ALLOCATION
 };
 
 #endif // __IEVENT_H__
